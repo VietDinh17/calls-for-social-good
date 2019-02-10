19ce0fa2d9134e5731520dcabf4bc27e52faf4cb
Eddie <42883353+Kaleidics@users.noreply.github.com>
2019-02-10 01:37:47 -0800

    update styles

--- src/loginPage/LoginPage.js
+++ src/loginPage/LoginPage.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
-import './LoginPage.css'
+import './LoginPage.css';
+import {Link} from 'react-router-dom'
 
 class LoginPage extends React.Component{
     render(){
         return(
+            <div>
+                <Link to="/caller">
                 <div className="login-select">
                     <div className="caller-select">
                         <div className="inner-call"><img class="john" src="https://i.imgur.com/2UN3RmG.jpg"></img><h2 className="c-btn">John <br/>Caller</h2></div>
@@ -12,6 +15,8 @@ class LoginPage extends React.Component{
                     <div className="callee-select">
                         <div className="inner-call"><img class="mary" src="https://i.imgur.com/mjffNQA.jpg"></img><h2 className="c2-btn">Mary <br/>Call-ee</h2></div>
                     </div>
+            </div>
+            </Link>
             </div>
             
         )
